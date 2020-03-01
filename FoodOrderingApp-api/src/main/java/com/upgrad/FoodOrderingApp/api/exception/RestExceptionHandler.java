b35f2d0f13a21f453ffddf427a6908d1d9f2e4c0
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-03-01 15:52:01 +0530

    Added Authentication Failed Exception Handler

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/exception/RestExceptionHandler.java
@@ -2,6 +2,7 @@ package com.upgrad.FoodOrderingApp.api.exception;
 
 
 import com.upgrad.FoodOrderingApp.api.model.ErrorResponse;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,5 +19,11 @@ public class RestExceptionHandler {
     public ResponseEntity<ErrorResponse> signupRestrictedExceptionHandler(SignUpRestrictedException sgr, WebRequest request) {
         return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(sgr.getCode()).message(sgr.getErrorMessage())
                                                  .rootCause(sgr.getMessage()),HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(AuthenticationFailedException.class)
+    public ResponseEntity<ErrorResponse> authenticationFailedExceptionHandler(AuthenticationFailedException afe, WebRequest request) {
+        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(afe.getCode()).message(afe.getErrorMessage())
+                .rootCause(afe.getMessage()),HttpStatus.BAD_REQUEST);
     }
 }
