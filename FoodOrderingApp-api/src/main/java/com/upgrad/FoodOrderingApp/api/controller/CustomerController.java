be536ba6b2bf260201847e7052edd0c2a3c862f6
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-03-01 15:36:15 +0530

    Add method to verify Authorization Header Format

--- FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
+++ FoodOrderingApp-api/src/main/java/com/upgrad/FoodOrderingApp/api/controller/CustomerController.java
@@ -2,20 +2,19 @@ package com.upgrad.FoodOrderingApp.api.controller;
 
 import com.upgrad.FoodOrderingApp.api.mappers.RequestMapper;
 import com.upgrad.FoodOrderingApp.api.mappers.ResponseMapper;
+import com.upgrad.FoodOrderingApp.api.model.LoginResponse;
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerRequest;
 import com.upgrad.FoodOrderingApp.api.model.SignupCustomerResponse;
 import com.upgrad.FoodOrderingApp.service.businness.CustomerService;
+import com.upgrad.FoodOrderingApp.service.businness.JwtTokenProvider;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping()
@@ -24,13 +23,26 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
+
     @RequestMapping(method = RequestMethod.POST, path = "/customer/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
     produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 
     public ResponseEntity<SignupCustomerResponse> signup(@RequestBody(required = false) final SignupCustomerRequest signupCustomerRequest)
-            throws SignUpRestrictedException {
+            throws SignUpRestrictedException, AuthenticationFailedException {
         CustomerEntity customerEntity = RequestMapper.toCustomerEntity(signupCustomerRequest);
         SignupCustomerResponse signupCustomerResponse = ResponseMapper.toSignupResponse(customerService.signupCustomer(customerEntity));
         return new ResponseEntity<SignupCustomerResponse>(signupCustomerResponse, HttpStatus.CREATED);
     }
+
+    @RequestMapping(method = RequestMethod.POST, path = "/customer/login", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<LoginResponse> login(@RequestHeader(name = "authorization") final String authorizationHeader)
+            throws AuthenticationFailedException {
+        CustomerEntity customerEntity;
+
+        // Check the format of the Authorization Header
+        customerService.verifyAuthorizationHeaderFormat(authorizationHeader);
+    }
+
 }
--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CustomerService.java
@@ -2,6 +2,7 @@ package com.upgrad.FoodOrderingApp.service.businness;
 
 import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
 import com.upgrad.FoodOrderingApp.service.entity.CustomerEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AuthenticationFailedException;
 import com.upgrad.FoodOrderingApp.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
+import java.util.regex.Pattern;
 
 @Service
 public class CustomerService {
@@ -19,15 +21,20 @@ public class CustomerService {
     @Autowired
     PasswordCryptographyProvider passwordEncryptor;
 
-    public CustomerEntity getCustomerWithPhoneNumber(String contactNumber) {
+    public CustomerEntity getCustomerWithPhoneNumber(String contactNumber) throws AuthenticationFailedException {
         CustomerEntity customerEntity = customerDao.getCustomerByPhone(contactNumber);
-        return customerEntity;
+
+        if (customerEntity == null) {
+            throw new AuthenticationFailedException("ATH-001", "This contact number has not been registered!");
+        } else {
+            return customerEntity;
+        }
 
     }
 
 
     @Transactional(propagation = Propagation.REQUIRED)
-    public CustomerEntity signupCustomer(CustomerEntity customerEntity) throws SignUpRestrictedException {
+    public CustomerEntity signupCustomer(CustomerEntity customerEntity) throws SignUpRestrictedException, AuthenticationFailedException {
         CustomerEntity existingCustomer = getCustomerWithPhoneNumber(customerEntity.getContactNumber());
         if(existingCustomer!=null){
             throw new SignUpRestrictedException("SGR-001","This contact number is already registered! Try other contact number.");
@@ -39,6 +46,14 @@ public class CustomerService {
         customerEntity.setUuid(UUID.randomUUID().toString());
         return customerDao.signupCustomer(customerEntity);
     }
+
+    public void verifyAuthorizationHeaderFormat(final String authorizationHeader)
+            throws AuthenticationFailedException {
+        if (!Pattern.matches("Basic \\d{3,30}:\\S{3,255}", authorizationHeader)) {
+            throw new AuthenticationFailedException("ATH-003", "Incorrect format of decoded customer name and password");
+
+        }
+
+    }
+
 }
-
-
