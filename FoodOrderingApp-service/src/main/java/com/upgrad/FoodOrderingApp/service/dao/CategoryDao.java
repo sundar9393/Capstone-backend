cc109f73b3460bb0884d12539a5335f9c6a286d0
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-03-07 00:10:49 +0530

    Added Category DAO

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/dao/CategoryDao.java
@@ -1,8 +1,26 @@
 package com.upgrad.FoodOrderingApp.service.dao;
 
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
 @Repository
 public class CategoryDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<CategoryEntity> getAllCategories() {
+        try {
+            return entityManager.createNamedQuery("getAllCategories", CategoryEntity.class).getResultList();
+        } catch (NoResultException nre) {
+            return null;
+        }
+
+    }
 
 }
