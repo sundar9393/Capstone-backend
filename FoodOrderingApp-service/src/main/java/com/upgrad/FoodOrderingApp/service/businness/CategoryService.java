7ed142fa98a7ea4e4d2686ecc9ba223f413a3925
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-03-07 00:11:45 +0530

    Added Category Service

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -1,8 +1,28 @@
 package com.upgrad.FoodOrderingApp.service.businness;
 
+import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
+import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
+import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
+import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.util.ServiceUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+
+    @Autowired
+    CustomerDao customerDao;
+
+    @Autowired
+    CategoryDao categoryDao;
+
+    public List<CategoryEntity> getAllCategories(String accessToken) throws AuthorizationFailedException {
+        ServiceUtil.validateAuthToken(customerDao.getAuthTokenWithAccessToken(accessToken));
+
+        return categoryDao.getAllCategories();
+    }
 
 }
