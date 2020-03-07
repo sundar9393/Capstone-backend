c9fccfa4ed9c7f025b57a118960f0f795ee07ed6
Suraj Sulladmath <suraj.sulladmath@sprinklr.com>
2020-03-07 14:32:34 +0530

    Added Category Service Method to retrieve Category By UUID

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/CategoryService.java
@@ -4,6 +4,7 @@ import com.upgrad.FoodOrderingApp.service.dao.CategoryDao;
 import com.upgrad.FoodOrderingApp.service.dao.CustomerDao;
 import com.upgrad.FoodOrderingApp.service.entity.CategoryEntity;
 import com.upgrad.FoodOrderingApp.service.exception.AuthorizationFailedException;
+import com.upgrad.FoodOrderingApp.service.exception.CategoryNotFoundException;
 import com.upgrad.FoodOrderingApp.service.util.ServiceUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,25 @@ public class CategoryService {
         ServiceUtil.validateAuthToken(customerDao.getAuthTokenWithAccessToken(accessToken));
 
         return categoryDao.getAllCategories();
+    }
+
+    public CategoryEntity getCategoryByUuid(String accessToken, String uuid) throws AuthorizationFailedException,
+            CategoryNotFoundException {
+        ServiceUtil.validateAuthToken(customerDao.getAuthTokenWithAccessToken(accessToken));
+
+        if (uuid.isEmpty()) {
+            throw new CategoryNotFoundException("CNF-001", "Category id field should not be empty");
+        } else {
+            CategoryEntity categoryEntity = categoryDao.getCategoryByUuid(uuid);
+
+            if (categoryEntity == null) {
+                throw new CategoryNotFoundException("CNF-002", "No category by this id");
+            } else {
+                return categoryEntity;
+            }
+
+        }
+
     }
 
 }
